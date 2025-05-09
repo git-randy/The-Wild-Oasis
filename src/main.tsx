50e398b766b9cd7e1b0886baf080fba5567329fe
AlphaTangoFoxtrot <randyryee@gmail.com>
2025-05-09 17:34:58 -0400

    Removed ErrorBoundary from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,11 @@ import App from "./App.tsx";
 // https://github.com/styled-components/styled-components/issues/4049
 import { StyleSheetManager } from "styled-components";
 import isPropValid from "@emotion/is-prop-valid";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./ui/ErrorFallback.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StyleSheetManager shouldForwardProp={isPropValid}>
     <StrictMode>
-      <ErrorBoundary
-        FallbackComponent={ErrorFallback}
-        onReset={() => window.location.replace("/")}
-      >
         <App />
-      </ErrorBoundary>
     </StrictMode>
   </StyleSheetManager>
 );
